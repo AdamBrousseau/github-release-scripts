1b697dbc2952629220030953c80c686e54e0b7fe
Adam Brousseau <adam.brousseau88@gmail.com>
2021-08-03 13:38:09 -0400

    fix versionNumber
    Signed-off-by: Adam Brousseau <adam.brousseau88@gmail.com>

--- sbin/Release.sh
+++ sbin/Release.sh
@@ -84,7 +84,7 @@ echo "Release: $RELEASE"
 
 RELEASE_OPTION=""
 if [ "$RELEASE" == "true" ]; then
-    versionNumber=${VERSION#"jdk"}
+    versionNumber=${VERSION#"JDK"}
   description="Official Release of IBM Semeru Runtime Open Edition for Java $versionNumber with Eclipse OpenJ9 $TAG
 License: GPL v2 with Classpath exception
 Certification: No"
